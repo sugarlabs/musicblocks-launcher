93427bd3c6400a5d602987fa8fee10936c920d08
Christopher Liu <christopherliu@localhost.localdomain>
2020-07-22 11:07:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import gi
+import os
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('WebKit2', '4.0')
+from gi.repository import Gtk
+from gi.repository import WebKit2
+
+WebKit2.WebView.__gtype__
+WebKit2.Settings.__gtype__
+
+@Gtk.Template(filename='webview.ui')
+class MyWindow(Gtk.Window):
+    __gtype_name__ = "MyWindow"
+    
+    view = Gtk.Template.Child()
+    settings = Gtk.Template.Child()
+    
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        print(self.view)
+        
+        
+win = MyWindow()
+win.set_title('Dragon Blocks')
+win.view.load_uri('file:///app/bin/index.html')
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
