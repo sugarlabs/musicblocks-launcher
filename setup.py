bc3157a74b64258f3ac480880153253cff27194b
Christopher Liu <cliu20@stuy.edu>
2020-07-27 12:39:26 -0400

    add --device=dri perms and update developer name

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(name='Music Blocks Launcher',
     version='1.0',
     description='Flatpak launcher for Music Blocks',
     url="https://github.com/sugarlabs/musicblocks-launcher",
-    author="Christopher Liu",
-    author_email="christopherliu@sorcero.com",
     data_files=[
         ('share/metainfo', ['data/org.sugarlabs.MusicBlocks.appdata.xml']),
         ('share/icons/hicolor/48x48/apps', ['data/org.sugarlabs.MusicBlocks.svg']),
