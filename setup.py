9b9a6f6f14ea861b395eb6c5448604cf7b5b5278
Christopher Liu <cliu20@stuy.edu>
2020-07-28 17:09:16 -0400

    change icon size to 128x128

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='Music Blocks Launcher',
     url="https://github.com/sugarlabs/musicblocks-launcher",
     data_files=[
         ('share/metainfo', ['data/org.sugarlabs.MusicBlocks.appdata.xml']),
-        ('share/icons/hicolor/48x48/apps', ['data/org.sugarlabs.MusicBlocks.svg']),
+        ('share/icons/hicolor/128x128/apps', ['data/org.sugarlabs.MusicBlocks.svg']),
         ('share/applications', ['data/org.sugarlabs.MusicBlocks.desktop']),
         ('share/musicblocks', ['data/org.sugarlabs.MusicBlocks.ui']),
         ('bin', ['launcher'])
