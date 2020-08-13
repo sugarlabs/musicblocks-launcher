3806873d64050b0dda78137ddc252b5fcb0cb9ea
Christopher Liu <cliu20@stuy.edu>
2020-08-13 15:16:52 -0400

    move binaries to musicblocks-launcher

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from distutils.core import setup
 import os
+import platform
+from zipfile import Zipfile
+
+arch = platform.machine()
+if arch == 'aarch64':
+    with Zipfile('build/MusicBlocks-aarch64.zip', 'r') as zipObj:
+        zipObj.extractall('build')
+        os.rename('build/MusicBlocks-aarch64', 'build/MusicBlocks')
+else if arch == 'arm':
+    with Zipfile('build/MusicBlocks-arm.zip', 'r') as zipObj:
+        zipObj.extractall('build')
+        os.rename('build/MusicBlocks-arm', 'build/MusicBlocks')
+else if arch == 'x86_64':
+    with Zipfile('build/MusicBlocks-x86_64.zip', 'r') as zipObj:
+        zipObj.extractall('build')
+        os.rename('build/MusicBlocks-x86_64', 'build/MusicBlocks')
+
 
 setup(name='Music Blocks Launcher',
     version='1.1.1',
@@ -9,5 +26,6 @@ setup(name='Music Blocks Launcher',
         ('share/metadata', ['data/org.sugarlabs.MusicBlocks.appdata.xml']),
         ('share/icons/hicolor/scalable/apps', ['data/org.sugarlabs.MusicBlocks.svg']),
         ('share/applications', ['data/org.sugarlabs.MusicBlocks.desktop']),
+        ('bin', ['build/MusicBlocks'])
         ]
     )
