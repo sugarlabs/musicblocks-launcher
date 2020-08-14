3d71d29f500f320b4f20e5fa14be5554c0c43b8f
Christopher Liu <cliu20@stuy.edu>
2020-08-14 11:03:41 -0400

    use AppImage for app binary

--- setup.py
+++ setup.py
@@ -1,23 +1,5 @@
 from distutils.core import setup
 import os
-import platform
-from zipfile import ZipFile
-
-arch = platform.machine()
-if arch == 'aarch64':
-    with ZipFile('build/MusicBlocks-aarch64.zip', 'r') as zipObj:
-        zipObj.extractall('build')
-        os.rename('build/MusicBlocks-aarch64', 'build/MusicBlocks')
-elif arch == 'x86_64':
-    with ZipFile('build/MusicBlocks-x86_64.zip', 'r') as zipObj:
-        zipObj.extractall('build')
-        os.rename('build/MusicBlocks-x86_64', 'build/MusicBlocks')
-else:
-    with ZipFile('build/MusicBlocks-arm.zip', 'r') as zipObj:
-        zipObj.extractall('build')
-        os.rename('build/MusicBlocks-arm', 'build/MusicBlocks')
-
-os.chmod('build/MusicBlocks', 0o755)
 
 setup(name='Music Blocks Launcher',
     version='1.1.1',
