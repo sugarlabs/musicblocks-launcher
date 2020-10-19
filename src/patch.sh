4961fdcbbcb7c53f3322e47207b6e712f479821e
Srevin Saju <srevinsaju@sugarlabs.org>
2020-10-19 19:50:18 +0300

    fix: regression introduced by https://github.com/sugarlabs/musicblocks/commit/b54b9512e7bea4a078ed56aacb01625a90645417

--- src/patch.sh
+++ src/patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i "s,window.isElectron == true,true,g" musicblocks/js/*.js
-sed -i "s,confirm(,false \&\& confirm(,g" musicblocks/js/SaveInterface.js
-sed -i "s,e.preventDefault();,,g" musicblocks/js/SaveInterface.js
-sed -i 's,e.returnValue = "";,,g' musicblocks/js/SaveInterface.js
+sed -i "s,return \"\";,,g" musicblocks/js/SaveInterface.js
+sed -i "s,event.preventDefault();,,g" musicblocks/js/SaveInterface.js
+sed -i 's,event.returnValue = "";,,g' musicblocks/js/SaveInterface.js
 
