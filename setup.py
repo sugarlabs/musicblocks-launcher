1653ff05102823e59b8a266ac2bab56df0a88d39
Christopher Liu <christopherliu@sorcero.com>
2021-06-22 22:42:42 -0400

    Bump launcher version number to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import os
 
 setup(name='Music Blocks Launcher',
-    version='1.1.4',
+    version='1.2.0',
     description='Flatpak launcher for Music Blocks',
     url="https://github.com/sugarlabs/musicblocks-launcher",
     data_files=[
