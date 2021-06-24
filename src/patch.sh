e769399321bad6f9af99bb97d13098f4b749f4d6
Christopher Liu <christopherliu@sorcero.com>
2021-06-24 12:42:32 -0400

    fix: Only replace title in activity.js

--- src/patch.sh
+++ src/patch.sh
@@ -3,4 +3,4 @@ sed -i "s,window.isElectron == true,true,g" musicblocks/js/*.js
 sed -i "s,return \"\";,,g" musicblocks/js/SaveInterface.js
 sed -i "s,event.preventDefault();,,g" musicblocks/js/SaveInterface.js
 sed -i 's,event.returnValue = "";,,g' musicblocks/js/SaveInterface.js
-sed -i 's, is a collection of tools for exploring fundamental musical concepts in a fun way.,,g' musicblocks/js/turtledefs.js
+sed -i 's,document.title = TITLESTRING;,document.title = "Music Blocks",g' musicblocks/js/activity.js
