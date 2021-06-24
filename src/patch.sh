da2a0324d0474472a45d0434ffc232c935b60c73
Christopher Liu <christopherliu@sorcero.com>
2021-06-24 12:13:42 -0400

    Replace TITLESTRING in patch

--- src/patch.sh
+++ src/patch.sh
@@ -3,5 +3,4 @@ sed -i "s,window.isElectron == true,true,g" musicblocks/js/*.js
 sed -i "s,return \"\";,,g" musicblocks/js/SaveInterface.js
 sed -i "s,event.preventDefault();,,g" musicblocks/js/SaveInterface.js
 sed -i 's,event.returnValue = "";,,g' musicblocks/js/SaveInterface.js
-sed -i 's, is a collection of tools for exploring musical,,g' musicblocks/index.html
-sed -i 's,concepts.,,g' musicblocks/index.html
+sed -i 's, is a collection of tools for exploring fundamental musical concepts in a fun way.,,g' musicblocks/js/turtledefs.js
